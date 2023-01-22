5c26f94e21101a75184d4f6e232ffa1795f5c3e6
Kurt Vermillion <kurtv0727@gmail.com>
2023-01-21 23:12:07 -0500

    changed seeds

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,14 +6,6 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
   
-  get '/projects' do
-    Project.all.to_json(include: :items)
-  end
-
-  get '/items' do
-    Item.all.to_json
-  end
-
   get '/customers' do
     Customer.all.to_json(include: {projects: {include: :items}})
   end
@@ -27,8 +19,20 @@ class ApplicationController < Sinatra::Base
     customer.projects.to_json(include: :items)
   end
 
+  get '/projects' do
+    Project.all.to_json(include: :items)
+  end
+
   get '/projects/:id' do
     Project.find(params[:id]).to_json(include: :items)
+  end
+
+  get '/items' do
+    Item.all.to_json
+  end
+
+  get 'items/:id' do
+    Item.find(params[:id]).to_json
   end
 
   post '/customers' do
@@ -65,13 +69,31 @@ class ApplicationController < Sinatra::Base
   delete '/projects/:id' do
     project = Project.find(params[:id])
     project.destroy
-    project.to_json
+    project.to_json(include: :items)
   end
 
   delete '/customers/:id' do
     customer = Customer.find(params[:id])
     customer.destroy
     customer.to_json
+  end
+
+  delete '/items/:id' do
+    item = Item.find(params[:id])
+    item.destroy
+    item.to_json
+  end
+
+  patch '/items/:id' do
+    item = Item.find(params[:id])
+    item.update(
+      item_name: params[:item_name],
+      item_cost: params[:item_cost],
+      project_id: params[:project_id],
+      project_category: params[:project_category],
+      quantity: params[:quantity]
+    )
+    item.to_json
   end
 
 end
